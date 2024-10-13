6fe5366dccd730a8d111ac22089a11a60e498604
ProjectEmu <steve.grilliot@gmail.com>
2024-10-12 17:58:33 -0700

    Finished Project

--- asteroid.py
+++ asteroid.py
@@ -12,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        splitangle = random.uniform(20, 50)
+        v_1 = self.velocity.rotate(splitangle)
+        v_2 = self.velocity.rotate(-splitangle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity = v_1 * 1.2
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2.velocity = v_2 * 1.2
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -15,8 +16,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers= (updatable, drawable)
+
+    Shot.containers = (shots, updatable, drawable)
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers= updatable
@@ -26,10 +30,6 @@ def main():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-
-
-
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -37,6 +37,15 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                print("Game over!")
+                exit()
+            for shot in shots:
+                if shot.collision(asteroid):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
